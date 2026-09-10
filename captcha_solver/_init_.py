f33e5623b1fc0a4578cb049d142e955014480489
Theskylist <2210733217@qq.com>
2026-09-10 21:38:41 +0800

    Update _init_.py

--- captcha_solver/_init_.py
+++ captcha_solver/_init_.py
@@ -1,1 +1,11 @@
+# Text-Click Captcha Solver
+# 文字顺序点击验证码自动破解
+# Version: 1.0.0
 
+from .solver import TextClickCaptchaSolver, solve_clickword, extract_words_from_prompt
+
+__all__ = [
+    "TextClickCaptchaSolver",
+    "solve_clickword",
+    "extract_words_from_prompt",
+]
